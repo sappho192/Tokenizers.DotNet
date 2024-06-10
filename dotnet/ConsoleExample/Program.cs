2529f0945f21e4b69ab3988563c8330888115ba6
TAE-IN Sappho KIM <sappho192@gmail.com>
2024-06-10 16:23:46 +0900

    [WIP] Implement dotnet project

---
+++ dotnet/ConsoleExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tokenizers.DotNet;
+
+Console.WriteLine("Hello, World!");
+
+// Download skt/kogpt2-base-v2/tokenizer.json from the hub
+var hubName = "skt/kogpt2-base-v2";
+var filePath = "tokenizer.json";
+var fileFullPath = await HuggingFace.GetFileFromHub(hubName, filePath, "deps");
+Console.WriteLine($"Downloaded {fileFullPath}");
+
+// Write the path of tokenizer.json to deps/tokenizer.path.txt
+var tokenizerPath = "tokenizer.path.txt";
+await File.WriteAllTextAsync(tokenizerPath, fileFullPath);
+Console.WriteLine($"Wrote {fileFullPath} to {tokenizerPath}");
+
+// Create a tokenizer instance
+var tokenizer = new Tokenizer();
+var tokens = new uint[] { 9330, 387, 12857, 9376, 18649, 9098, 7656, 6969, 8084, 1 };
+var decoded = tokenizer.Decode(tokens);
+Console.WriteLine($"Decoded: {decoded}");
+
