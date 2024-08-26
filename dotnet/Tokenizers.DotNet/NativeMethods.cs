87fb1ae708306d45d0e1be1c56a9380f44b3b4dc
Taein KIM <sappho192@gmail.com>
2024-08-26 10:23:09 +0900

    Upgrade csbindgen to 1.9.3

--- dotnet/Tokenizers.DotNet/NativeMethods.cs
+++ dotnet/Tokenizers.DotNet/NativeMethods.cs
@@ -17,28 +17,28 @@ namespace CsBindgen
 
 
         [DllImport(__DllName, EntryPoint = "alloc_u8_string", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern ByteBuffer* alloc_u8_string();
+        internal static extern ByteBuffer* alloc_u8_string();
 
         [DllImport(__DllName, EntryPoint = "free_u8_string", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void free_u8_string(ByteBuffer* buffer);
+        internal static extern void free_u8_string(ByteBuffer* buffer);
 
         [DllImport(__DllName, EntryPoint = "csharp_to_rust_string", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void csharp_to_rust_string(ushort* utf16_str, int utf16_len);
+        internal static extern void csharp_to_rust_string(ushort* utf16_str, int utf16_len);
 
         [DllImport(__DllName, EntryPoint = "csharp_to_rust_u32_array", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void csharp_to_rust_u32_array(uint* buffer, int len);
+        internal static extern void csharp_to_rust_u32_array(uint* buffer, int len);
 
         [DllImport(__DllName, EntryPoint = "tokenizer_initialize", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern ByteBuffer* tokenizer_initialize(ushort* utf16_path, int utf16_path_len);
+        internal static extern ByteBuffer* tokenizer_initialize(ushort* utf16_path, int utf16_path_len);
 
         [DllImport(__DllName, EntryPoint = "tokenizer_encode", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern ByteBuffer* tokenizer_encode(ushort* _session_id, int _session_id_len, ushort* _text, int _text_len);
+        internal static extern ByteBuffer* tokenizer_encode(ushort* _session_id, int _session_id_len, ushort* _text, int _text_len);
 
         [DllImport(__DllName, EntryPoint = "tokenizer_decode", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern ByteBuffer* tokenizer_decode(ushort* _session_id, int _session_id_len, uint* _token_ids, int _token_ids_len);
+        internal static extern ByteBuffer* tokenizer_decode(ushort* _session_id, int _session_id_len, uint* _token_ids, int _token_ids_len);
 
         [DllImport(__DllName, EntryPoint = "get_version", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern ByteBuffer* get_version(ushort* _session_id, int _session_id_len);
+        internal static extern ByteBuffer* get_version(ushort* _session_id, int _session_id_len);
 
 
     }
