da72600197e2c1eb6864081d9d34366a7edd9735
Noah Czaplewski <Noah.Czaplewski@biexcellence.com>
2025-03-28 09:44:50 +0100

    Added linux build scripts

---
+++ pack.sh
@@ -0,0 +1,36 @@
+﻿#!/bin/bash
+
+set -e
+base_dir="nuget"
+
+# Retrieve the current Git branch and commit hash
+branch=$(git rev-parse --abbrev-ref HEAD)
+commit=$(git rev-parse HEAD)
+
+# Iterate over each subdirectory in the base directory
+for dir in "$base_dir"/*/; do
+    # Check if it is a directory
+    if [[ -d "$dir" ]]; then
+        echo "Entering directory: $dir"
+        
+        # Change to the subdirectory
+        pushd "$dir" > /dev/null
+        
+        # Find the first .nuspec file in the current subdirectory
+        nuspec_file=$(find . -maxdepth 1 -type f -name "*.nuspec" | head -n 1)
+        
+        # Check if a .nuspec file was found
+        if [[ -n "$nuspec_file" ]]; then
+            echo "Found nuspec file: $nuspec_file"
+
+            nuget pack "$nuspec_file"
+        else
+            echo "No .nuspec file found in $dir"
+        fi
+        
+        # Return to the original directory
+        popd > /dev/null
+    else
+        echo "$dir is not a valid directory."
+    fi
+done
---
+++ update_version.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+
+version=$(cat "./NATIVE_LIB_VERSION.txt")
+echo "Version: $version"
+
+sed -E 's/^(version\s*=\s*")[^"]*/\1'"$version"'/' "rust/Cargo.toml"
+
+# Update version in every nuspec
+find nuget -type f -name "*.nuspec" | while IFS= read -r nuspecFile; do
+    echo "Processing: $nuspecFile"
+    # Use sed to replace the content within the <version> tags
+    sed -i.bak -E "s/(<version>)[^<]*(<\/version>)/\1$version\2/" "$nuspecFile"
+done
