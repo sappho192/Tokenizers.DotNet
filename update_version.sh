73fa9f841fa55772bf5ef09924811779fbeb7ad5
Noah Czaplewski <Noah.Czaplewski@biexcellence.com>
2025-03-28 10:57:39 +0100

    Updated update_version.sh script

--- update_version.sh
+++ update_version.sh
@@ -9,5 +9,5 @@ sed -E 's/^(version\s*=\s*")[^"]*/\1'"$version"'/' "rust/Cargo.toml"
 find nuget -type f -name "*.nuspec" | while IFS= read -r nuspecFile; do
     echo "Processing: $nuspecFile"
     # Use sed to replace the content within the <version> tags
-    sed -i.bak -E "s/(<version>)[^<]*(<\/version>)/\1$version\2/" "$nuspecFile"
+    sed -i "s/(<version>)[^<]*(<\/version>)/\1$version\2/" "$nuspecFile"
 done
