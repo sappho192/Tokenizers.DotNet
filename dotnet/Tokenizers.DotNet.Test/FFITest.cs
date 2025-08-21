5ec45674b73eb2a70c59340fcdc2609bc262abf1
Aristarkh Zagorodnikov <xm@x-infinity.com>
2025-08-21 13:31:42 +0100

    test: use TokenizerFixture for FFITest

--- dotnet/Tokenizers.DotNet.Test/FFITest.cs
+++ dotnet/Tokenizers.DotNet.Test/FFITest.cs
@@ -6,20 +6,25 @@ using System.Text;
 
 namespace Tokenizers.DotNet.Test
 {
+    [Collection(TokenizerCollection.Name)]
     public class FFITest
     {
-        [Fact]
-        public async Task CheckInitialization()
+        private readonly TokenizerFixture _model;
+
+        public FFITest(TokenizerFixture model)
         {
-            var hubName = "skt/kogpt2-base-v2";
-            var filePath = "tokenizer.json";
-            var fileFullPath = await HuggingFace.GetFileFromHub(hubName, filePath, "deps");
+            _model = model ?? throw new ArgumentNullException(nameof(model));
+        }
+
+        [Fact]
+        public void CheckInitialization()
+        {
             var sessionId = string.Empty;
             unsafe
             {
-                fixed (char* p = fileFullPath)
+                fixed (char* p = _model.FilePath)
                 {
-                    var tokenizerResult = NativeMethods.tokenizer_initialize((ushort*)p, fileFullPath.Length);
+                    var tokenizerResult = NativeMethods.tokenizer_initialize((ushort*)p, _model.FilePath.Length);
                     Assert.Equal(TokenizerErrorCode.Success, tokenizerResult.error_code);
                     var str = Encoding.UTF8.GetString(tokenizerResult.data->AsSpan());
                     sessionId = new string(str);
