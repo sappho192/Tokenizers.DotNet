6660e5d96ea6ca1bc98fed02a98f376b8b5e8185
Aristarkh Zagorodnikov <xm@x-infinity.com>
2025-08-21 17:14:29 +0100

    test: simplify TokenizerFixture significantly, utilizing the in-repository models

--- dotnet/Tokenizers.DotNet.Test/Internals/TokenizerFixture.cs
+++ dotnet/Tokenizers.DotNet.Test/Internals/TokenizerFixture.cs
@@ -1,67 +1,20 @@
 ﻿namespace Tokenizers.DotNet.Test;
 
-public sealed class TokenizerFixture : IAsyncLifetime
+public sealed class TokenizerFixture
 {
-    private const string FileName = "tokenizer.json";
+    private const string ModelPath = "../../../../../test-assets/tokenizers";
     private const string ModelName = "skt/kogpt2-base-v2";
-
-    private static readonly SemaphoreSlim Lock = new(1);
-    private static readonly string ModelFileName = GetModelFileName();
+    private const string FileName = "tokenizer.json";
 
     private readonly Lazy<Tokenizer> _tokenizer;
-    private string? _filePath;
 
     public TokenizerFixture()
     {
+        FilePath = Path.GetFullPath(Path.Combine(ModelPath, ModelName, FileName));
         _tokenizer = new(() => new(FilePath));
     }
 
-    public string FilePath => _filePath ?? throw new InvalidOperationException("Model path is not initialized.");
+    public string FilePath { get; }
 
     public Tokenizer Tokenizer => _tokenizer.Value;
-
-    public Task DisposeAsync() => Task.CompletedTask;
-
-    public async Task InitializeAsync()
-    {
-        if (_filePath is not null)
-        {
-            return;
-        }
-
-
-        var tempPath = Path.GetTempPath() + "Tokenizers.DotNet.Test";
-        var filePath = Path.Combine(tempPath, ModelFileName);
-        if (!File.Exists(filePath))
-        {
-            await Download(tempPath, filePath);
-        }
-
-        _filePath = filePath;
-    }
-
-    private static async Task Download(string tempPath, string filePath)
-    {
-        Directory.CreateDirectory(tempPath);
-
-        // Locking prevents multiple threads from downloading the file simultaneously, crashing tests, it may impede parallelism, but we really don't care for realistic scenarios
-        await Lock.WaitAsync();
-        try
-        {
-            // Download to temporary file and then atomically move to prevent broken files
-            File.Delete(filePath);
-            var tempFile = await HuggingFace.GetFileFromHub(ModelName, FileName, tempPath);
-            File.Move(tempFile, filePath);
-        }
-        finally
-        {
-            Lock.Release();
-        }
-    }
-
-    private static string GetModelFileName()
-    {
-        var invalidChars = Path.GetInvalidFileNameChars();
-        return new string([.. ModelName.Select(c => Array.IndexOf(invalidChars, c) < 0 ? c : '_')]) + "-" + FileName;
-    }
 }
