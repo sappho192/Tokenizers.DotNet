e1b2459ccc465a78bf58dd6860f55cac17528c1a
Aristarkh Zagorodnikov <xm@x-infinity.com>
2025-08-21 17:42:17 +0100

    test: add Models and ModelId to allow choosing different models and getting paths

---
+++ dotnet/Tokenizers.DotNet.Test/Internals/Models.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+
+namespace Tokenizers.DotNet.Test;
+
+internal static class Models
+{
+    private const string ModelPath = "../../../../../test-assets/tokenizers";
+    private const string FileName = "tokenizer.json";
+
+    private static readonly Func<ModelId, string> PathFactory =
+        id =>
+        {
+            var modelName = id switch
+            {
+                ModelId.KoGpt2 => "skt/kogpt2-base-v2",
+                ModelId.OaiGpt2 => "openai-community/gpt2",
+                _ => throw new ArgumentOutOfRangeException(nameof(id), id, null)
+            };
+
+            return Path.Combine(ModelPath, modelName, FileName);
+        };
+
+    private static readonly ConcurrentDictionary<ModelId, string> Paths = new();
+
+    public static string GetFilePath(ModelId id) => Paths.GetOrAdd(id, PathFactory);
+}
